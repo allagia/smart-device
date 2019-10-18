2ddd8851f853d9be79a05b142d5cfee1fa1978e4
Alla Panova <alla.mixalvna@gmail.com>
2019-10-18 16:40:31 +0300

    добавляет исправления по баглисту(часть5)

--- source/js/footer-mobile.js
+++ source/js/footer-mobile.js
@@ -12,21 +12,47 @@
       footerNavList.classList.add('footer-nav__list--opened');
       buttonNav.classList.add('footer-nav__button--opened');
     } else {
-      footerNavList.classList.add('footer-nav__list--closed');
       footerNavList.classList.remove('footer-nav__list--opened');
+      footerNavList.classList.add('footer-nav__list--closed');
       buttonNav.classList.remove('footer-nav__button--opened');
     }
   });
 
-  buttonContacts.addEventListener('click', function () {
+  buttonNav.addEventListener('click', function () {
     if (footerContacts.classList.contains('contacts__content--opened')) {
       footerContacts.classList.remove('contacts__content--opened');
       footerContacts.classList.add('contacts__content--closed');
       buttonContacts.classList.remove('contacts__button--opened');
-    } else {
-      footerContacts.classList.add('contacts__content--opened');
+      if (footerNavList.classList.contains('footer-nav__list--closed')) {
+        footerNavList.classList.remove('footer-nav__list--closed');
+        footerNavList.classList.add('footer-nav__list--opened');
+        buttonNav.classList.add('footer-nav__button--opened');
+      }
+    }
+  });
+
+  buttonContacts.addEventListener('click', function () {
+    if (footerContacts.classList.contains('contacts__content--closed')) {
       footerContacts.classList.remove('contacts__content--closed');
+      footerContacts.classList.add('contacts__content--opened');
       buttonContacts.classList.add('contacts__button--opened');
+    } else {
+      footerContacts.classList.add('contacts__content--closed');
+      footerContacts.classList.remove('contacts__content--opened');
+      buttonContacts.classList.remove('contacts__button--opened');
+    }
+  });
+
+  buttonContacts.addEventListener('click', function () {
+    if (footerNavList.classList.contains('footer-nav__list--opened')) {
+      footerNavList.classList.remove('footer-nav__list--opened');
+      footerNavList.classList.add('footer-nav__list--closed');
+      buttonNav.classList.remove('footer-nav__button--opened');
+      if (footerContacts.classList.contains('contacts__content--closed')) {
+        footerContacts.classList.remove('contacts__content--closed');
+        footerContacts.classList.add('contacts__content--opened');
+        buttonContacts.classList.add('contacts__button--opened');
+      }
     }
   });
 })();
