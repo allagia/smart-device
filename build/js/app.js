c2df6564a0e69db81aa918be9f8f305f9a5e4a10
Alla Panova <alla.mixalvna@gmail.com>
2019-10-21 14:25:24 +0300

    добавляет адаптив для попапа и центрует его на странице

--- build/js/app.js
+++ build/js/app.js
@@ -8,22 +8,26 @@
   var closePopup = popup.querySelector('.popup__close');
   var form = popup.querySelector('form');
   var name = form.querySelector('#id-name');
+  var body = document.querySelector('.general-body');
 
   button.addEventListener('click', function (evt) {
     evt.preventDefault();
     popup.classList.add('popup__show');
     name.focus();
+    body.classList.add('general-body--under-popup');
   });
 
   closePopup.addEventListener('click', function (evt) {
     evt.preventDefault();
     popup.classList.remove('popup__show');
+    body.classList.remove('general-body--under-popup');
   });
 
   closePopup.addEventListener('keydown', function (evt) {
     if (evt.keyCode === ENTER_KEYCODE) {
       evt.preventDefault();
       popup.classList.remove('popup__show');
+      body.classList.remove('general-body--under-popup');
     }
   });
 
@@ -32,6 +36,7 @@
       evt.preventDefault();
       if (popup.classList.contains('popup__show')) {
         popup.classList.remove('popup__show');
+        body.classList.remove('general-body--under-popup');
       }
     }
   });
